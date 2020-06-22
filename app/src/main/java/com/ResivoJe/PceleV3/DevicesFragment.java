c8825cfdf98873f04d10303756ec5fb759d0c0c7
Aleksandar Djordjevic <acasax@gmail.com>
2020-06-22 23:50:22 +0200

    Initial commit

---
+++ app/src/main/java/com/ResivoJe/PceleV3/DevicesFragment.java
@@ -0,0 +1,151 @@
+package com.ResivoJe.PceleV3;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class DevicesFragment extends ListFragment {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayList<BluetoothDevice> AllDevice = new ArrayList<>();
+    private ArrayList<BluetoothDevice> ValidDevice = new ArrayList<>();
+    private ArrayAdapter<BluetoothDevice> listAdapter;
+    Pattern sPattern = Pattern.compile("^BSram0000(\\d{2})$");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        if(getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH))
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    if(isValid(deviceName)){
+                        ValidDevice.add(device);
+                    }
+                }
+            }
+            listAdapter = new ArrayAdapter<BluetoothDevice>(getActivity(), 0, ValidDevice) {
+            @Override
+            public View getView(int p, View view, ViewGroup parent) {
+
+                    for (int i = 0; i < ValidDevice.size(); i++){
+                        if (view == null){
+                            view = getActivity().getLayoutInflater().inflate(R.layout.device_list_item, parent, false);
+                        }
+                        BluetoothDevice device = ValidDevice.get(i);
+                        TextView text1 = view.findViewById(R.id.text1);
+                        TextView text2 = view.findViewById(R.id.text2);
+                        text1.setText(device.getName());
+                        text2.setText(device.getAddress());
+                        return view;
+                    }
+                    return view;
+            }
+        };
+    }
+
+    boolean isValid(CharSequence s) {
+        return sPattern.matcher(s).matches();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setListAdapter(null);
+        View header = getActivity().getLayoutInflater().inflate(R.layout.device_list_header, null, false);
+        getListView().addHeaderView(header, null, false);
+        setEmptyText("initializing...");
+        ((TextView) getListView().getEmptyView()).setTextSize(18);
+        setListAdapter(listAdapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_devices, menu);
+        if(bluetoothAdapter == null)
+            menu.findItem(R.id.bt_settings).setEnabled(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(bluetoothAdapter == null)
+            setEmptyText("<Blutut nije dostupan na ovom urećaju>");
+        else if(!bluetoothAdapter.isEnabled())
+            setEmptyText("<Blutut je ugašen>");
+        else
+            setEmptyText("<Nema pronaćenih urećaja>");
+        refresh();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.bt_settings) {
+            Intent intent = new Intent();
+            intent.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void refresh() {
+        AllDevice.clear();
+        if(bluetoothAdapter != null) {
+            for (BluetoothDevice device : bluetoothAdapter.getBondedDevices())
+                if (device.getType() != BluetoothDevice.DEVICE_TYPE_LE)
+                    AllDevice.add(device);
+        }
+        Collections.sort(AllDevice, DevicesFragment::compareTo);
+        listAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        BluetoothDevice device = AllDevice.get(position-1);
+        Bundle args = new Bundle();
+        args.putString("device", device.getAddress());
+        Fragment fragment = new TerminalFragment();
+        fragment.setArguments(args);
+        getFragmentManager().beginTransaction().replace(R.id.fragment, fragment, "terminal").addToBackStack(null).commit();
+    }
+
+    /**
+     * sort by name, then address. sort named devices first
+     */
+    static int compareTo(BluetoothDevice a, BluetoothDevice b) {
+        boolean aValid = a.getName()!=null && !a.getName().isEmpty();
+        boolean bValid = b.getName()!=null && !b.getName().isEmpty();
+        if(aValid && bValid) {
+            int ret = a.getName().compareTo(b.getName());
+            if (ret != 0) return ret;
+            return a.getAddress().compareTo(b.getAddress());
+        }
+        if(aValid) return -1;
+        if(bValid) return +1;
+        return a.getAddress().compareTo(b.getAddress());
+    }
+}
---
+++ app/src/main/java/com/ResivoJe/PceleV3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ResivoJe.PceleV3;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new DevicesFragment(), "devices").commit();
+        else
+            onBackStackChanged();
+        toolbar.setTitleTextColor(Color.BLACK);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(getSupportFragmentManager().getBackStackEntryCount()>0);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
